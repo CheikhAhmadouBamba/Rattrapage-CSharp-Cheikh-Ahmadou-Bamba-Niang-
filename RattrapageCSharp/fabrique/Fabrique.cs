09f2204fa1ffca97cb9dc701c0a8defb4979ee7b
ahmadou <ahmadou-bamba.niang@ism.edu.sn>
2022-06-23 13:02:44 +0000

    csharp
    C Sharp

---
+++ RattrapageCSharp/fabrique/Fabrique.cs
@@ -0,0 +1,20 @@
+﻿using RattrapageCSharp.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RattrapageCSharp.fabrique
+{
+    class Fabrique
+    {
+        public static IService getInstanceService()
+        {
+            return new Service()
+            {
+                //Context = new MasterEntities(),
+            };
+        }
+    }
+}
---
+++ RattrapageCSharp/views/Home.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RattrapageCSharp.views
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListerComptes lc = new ListerComptes();
+            lc.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AjouterCompte ac = new AjouterCompte();
+            ac.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FaireDepot fp = new FaireDepot();
+            fp.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FaireRetrait fr = new FaireRetrait();
+            fr.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FaireConsultation fc = new FaireConsultation();
+            fc.Show();
+            this.Hide();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FaireVirement fv = new FaireVirement();
+            fv.Show();
+            this.Hide();
+        }
+    }
+}
